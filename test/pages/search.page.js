2d75e40bddafac36e39ab923bb2e8acd77a891bd
Ognjen Jegdic <ognjenjegdic@gmail.com>
2022-06-29 13:24:42 +0200

    added search page

--- test/pages/search.page.js
+++ test/pages/search.page.js
@@ -11,12 +11,31 @@ class SearchPage extends Page {
     };
 
     get savedSearchSections () {
-        return $$('h1[class*="index__sectionHeader"]')[1]
+        return $$('h1[class*="index__sectionHeader"]')[1];
+    };
+    get enteloSuggestsSections () {
+        return $$('h1[class*="index__sectionHeader"]')[2];
     };
 
-    get enteloSuggestsSections () {
-        return $$('h1[class*="index__sectionHeader"]')[2]
+    get searchField () {
+        return $('div[data-test="search-input"]');
     };
+
+    async waitForClick () {
+        return await $('ul[class*="styles__Autocomplete__items"]').waitForExist({timeout:5000});
+    }
+
+    get recentSearchFirstItem () {
+        return $$('ul[class*="styles__Autocomplete__items"]')[0].$('li[data-test="autocomplete-suggestion"]');
+    };
+
+    get suggestedSearchFirstItem () {
+        return $$('ul[class*="styles__Autocomplete__items"]')[1].$('li[data-test="autocomplete-suggestion"]');
+    };
+
+    get searchResultsWrapper () {
+        return $('ul[class*="index__SearchResults__profileList"]');
+    }
 
     open () {
         return super.open('search/new');
