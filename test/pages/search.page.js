5cd1e920eb9f2d21462c0acc1f86e235f7945c8f
Ognjen Jegdic <ognjenjegdic@gmail.com>
2022-07-01 11:23:15 +0200

    added more search page elements

--- test/pages/search.page.js
+++ test/pages/search.page.js
@@ -6,6 +6,12 @@ const Page = require('./page');
  */
 class SearchPage extends Page {
 
+
+    async waitForDropdownToBeOpened() {
+        return await $('ul[class*="styles__Autocomplete__items"]').waitForExist({timeout:10000});
+    }
+
+
     get recentSearchSections () {
         return $$('h1[class*="index__sectionHeader"]')[0]; 
     };
@@ -17,13 +23,13 @@ class SearchPage extends Page {
         return $$('h1[class*="index__sectionHeader"]')[2];
     };
 
+    get enteloSuggestsLink () {
+        return $('a[class*="suggestion"]');
+    }
+
     get searchField () {
         return $('div[data-test="search-input"]');
     };
-
-    async waitForClick () {
-        return await $('ul[class*="styles__Autocomplete__items"]').waitForExist({timeout:5000});
-    }
 
     get recentSearchFirstItem () {
         return $$('ul[class*="styles__Autocomplete__items"]')[0].$('li[data-test="autocomplete-suggestion"]');
@@ -35,6 +41,34 @@ class SearchPage extends Page {
 
     get searchResultsWrapper () {
         return $('ul[class*="index__SearchResults__profileList"]');
+    }
+
+    get searchBuilderButton () {
+        return $('button[data-test="search-builder-trigger"]');
+    }
+
+    get searchBuilderPopup () {
+        return $('main[class*="Builder__body"]')
+    }
+
+    get positionField () {
+        return $('input[type="text"]')
+    }
+
+    get positionSuggestion () {
+        return $('div[data-test="typeahead-suggestion"]');
+    }
+
+    get preferredExcludedPositionDropdown () {
+        return $$('div[class*="Select__select__label"]')[0];
+    }
+
+    get preferredExcludedSkillsDropdown () {
+        return $$('div[class*="Select__select__label"]')[1];
+    }
+
+    get preferredExcludedCompaniesDropdown () {
+        return $$('div[class*="Select__select__label"]')[0];
     }
 
     open () {
